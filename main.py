c605c11d43d543cc793826f3a5f9388b2128052c
MWANZIA <shem.masoo@student.moringaschool.com>
2023-12-20 08:59:18 +0300

    completed cake data

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ app = FastAPI()
 def index():
     return {"message":"My first Api"}
 
-
+#get all events
 @app.get('/cakes')
 def cakes():
      return[]
+#get a single event
+@app.get('/cakes/{cake_id}')
+def cakes():
+     return[]
+#create an event
 @app.post("/cakes")
 def create_cake():
      return {"message":"Data recieved"}
+#update an event
 @app.patch('/cakes/{cake_id}')
-def updated_event(cake_id:int):
+def updated_cake(cake_id:int):
      return{"message":f"Event{cake_id} created successfully"}
-
+#delete an event
 @app.delete('/cakes/{cake_id}')
-def delete_event(cake_id:int):
+def delete_cake(cake_id:int):
      return{"message":f"Event{cake_id}deleted"}
