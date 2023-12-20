81a737a667b83934ccffba1f1c2871fac15af83f
MWANZIA <shem.masoo@student.moringaschool.com>
2023-12-20 20:11:39 +0300

    json server

--- main.py
+++ main.py
@@ -19,13 +19,17 @@ def cakes(db :Session = Depends(get_db)):
      return cakes
 #get a single event
 @app.get('/cakes/{cake_id}')
-def cakes():
+def cakes(cake_id:int,db:Session= Depends(get_db)):
+     event = db.query(Cake),filter(Cake.id == cake_id)
      return[]
 #create an event
 @app.post("/cakes")
-def create_cake(cake: Cakedisplay):
-     print(cake)
-     return {"message":"Data recieved"}
+def create_cake(cake: Cakedisplay,db:Session = Depends(get_db)):
+     new_cake=(Cake(**cake.model_dump()))
+     db.add(new_cake)
+     db.commit()
+     db.refresh(new_cake)
+     return {"message":"Data recieved","cake":new_cake}
 #update an event
 @app.patch('/cakes/{cake_id}')
 def updated_cake(cake_id:int):
